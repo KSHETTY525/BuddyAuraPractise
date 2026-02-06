ea08edc2f653f3d40fdc8bbca936f76ea155a344
bhakti1755 <bhaktivyapari577@gmail.com>
2026-02-06 14:32:21 +0530

    discount and back button addeed

--- app/src/main/java/com/example/buddyaura/data/CartItem.kt
+++ app/src/main/java/com/example/buddyaura/data/CartItem.kt
@@ -3,6 +3,7 @@ package com.example.buddyaura.data
 data class CartItem(
     val name: String,
     val price: Int,
+
     var quantity: Int,
     val imageRes: Int,
     var isSelected: Boolean = false   // <-- add this line
